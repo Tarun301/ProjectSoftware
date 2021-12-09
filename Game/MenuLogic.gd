e6c747496d07b62e56ad31bc6265929be864cb7d
lhow42 <lhow42@hotmail.com>
2021-12-09 14:44:11 +1300

    Added instructions

--- Game/MenuLogic.gd
+++ Game/MenuLogic.gd
@@ -1,4 +1,5 @@
 extends Node
+onready var inst = get_node("../Instructions")
 
 func _on_Button_button_up():
 	get_tree().change_scene("res://World.tscn")
@@ -12,3 +13,8 @@ func _on_Button2_button_up():
 func _on_Button3_button_up():
 	Global.level = 2
 	get_tree().change_scene("res://Level 3.tscn")
+
+
+
+func _on_Button4_button_up():
+	inst.text = "Arrow keys  navigation\n ctrl to fire \ncollect keys to proceed"
